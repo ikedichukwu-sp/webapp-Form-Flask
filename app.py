4c95a86bc6bbb6a12bcdd36abd071ec0a6c54d2f
silaspaul ikedi <ikedichukwu1993@gmail.com>
2024-12-04 23:33:58 +0200

    created a database

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 from flask import Flask, render_template, request
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "myapp321"  # this secures the app from
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 
 @app.route("/", methods=["GET", "POST"])  # Handles both GET and POST
@@ -20,4 +32,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)  # Host updated for local network access
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)  # Host updated for local network access
